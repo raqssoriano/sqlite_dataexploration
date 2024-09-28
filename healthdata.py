3b405ef558d7af86eff73aa310b0ecc60bc0643f
raqssoriano <mrrsoriano@yahoo.com>
2024-09-28 19:04:25 -0400

    Initial commit

---
+++ healthdata.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import sqlite3
+
+
+### 1) I Searched and downloaded a Public Healthcare Dataset. I utilized dataset from Kaggle.com.
+
+df = pd.read_csv('/Users/raqsmacbookair/python/sqlite_dataexploration/mental_health_data.csv')
